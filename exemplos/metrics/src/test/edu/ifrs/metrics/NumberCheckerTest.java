ffcd54d6fec8aba51c05388f6a998795ac5133bb
guismoreira <guilhermemoreira55@gmail.com>
2021-09-01 22:16:08 -0300

    license

--- exemplos/metrics/src/test/edu/ifrs/metrics/NumberCheckerTest.java
+++ exemplos/metrics/src/test/edu/ifrs/metrics/NumberCheckerTest.java
@@ -1,3 +1,11 @@
+/**
+* PW2 by Rodrigo Prestes Machado e Guilheme Moreira
+* 
+* PW2 is licensed under a
+* Creative Commons Attribution 4.0 International License.
+* You should have received a copy of the license along with this
+* work. If not, see http://creativecommons.org/licenses/by/4.0/.
+*/
 package edu.ifrs.metrics;
 
 import static io.restassured.RestAssured.get;
